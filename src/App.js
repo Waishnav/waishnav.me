752dbb89af8998634c757e92800a3e22d210a8cf
Waishnav <waishnavdeore@gmail.com>
2022-11-07 20:33:02 +0530

    added posts | some global css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import NavigationBar from "./components/NavigationBar/NavigationBar"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+import Projects from "./pages/Projects.js";
+import Posts from "./pages/Posts.js";
 
 function App() {
   return (
-    <NavigationBar />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="posts" element={<Posts />} />
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/posts/delta-4-theory.js
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavigationBar/NavigationBar";
+import Footer from "../components/Footer/footer";
+
+function blog1() {
+  return (
+    <div>
+      <NavBar />
+      <Footer />
+    </div>
+  );
+}
+
+export default blog1;
