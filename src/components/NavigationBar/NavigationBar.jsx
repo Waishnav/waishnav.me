0f9a548ce0afb60106bb49308da8664721b43e0b
Waishnav <waishnavdeore@gmail.com>
2022-11-07 20:30:12 +0530

    modified navigation bar

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,56 +1,43 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Link } from "react-router-dom"
 
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Posts from "./pages/Posts";
-import Projects from "./pages/Projects";
-import About from "./pages/About";
-
-import "./NavigaitonBar.css"
+import "./NavigationBar.css"
 import { ReactComponent as HomeIcon } from "../../assets/home.svg";
 import { ReactComponent as PostsIcon } from "../../assets/posts.svg";
-import { ReactComponent as ProjectsIcon } from "../../assets/projects.svg.svg";
+import { ReactComponent as ProjectsIcon } from "../../assets/projects.svg";
 import { ReactComponent as AboutIcon } from "../../assets/about.svg";
 
-function NavigaitonBar() {
+function NavigatonBar() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <nav class="navbar">
-            <div class="navbox">
-              <div>
-                <Route path="/" element={<Home />}>
-                  <HomeIcon />
-                </Route>
-                <span>home</span>
-              </div>
-              <div>
-                <Route path="posts" element={<Posts />}>
-                  <PostsIcon />
-                </Route>
-                <span>posts</span>
-              </div>
-              <div>
-                <Route path="projects" element={<Projects />}>
-                  <ProjectsIcon />
-                </Route>
-                <span>projects</span>
-              </div>
-              <div>
-                <Route path="about" element={<About />}>
-                  <AboutIcon />
-                </Route>
-                <span>about</span>
-              </div>
-            </div>
-          </nav>
-        </Route>
-      </Routes>
-    </BrowserRouter >
+    <nav className="navbar">
+      <div className="navbox">
+        <div>
+          <Link className="nav-links" to="/">
+            <HomeIcon />
+          </Link>
+          <span>home</span>
+        </div>
+        <div>
+          <Link className="nav-links" to="/posts">
+            <PostsIcon />
+          </Link>
+          <span>posts</span>
+        </div>
+        <div>
+          <Link className="nav-links" to="/projects">
+            <ProjectsIcon />
+          </Link>
+          <span>projects</span>
+        </div>
+        <div>
+          <Link className="nav-links" to="/about">
+            <AboutIcon />
+          </Link>
+          <span>about</span>
+        </div>
+      </div>
+    </nav>
   )
-
 }
 
-export default NavigaitonBar
+export default NavigatonBar
