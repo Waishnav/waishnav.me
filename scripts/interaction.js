f04f9221e91865853334b9b9605059d248e96b09
Waishnav <waishnavdeore@gmail.com>
2022-12-24 00:06:58 +0530

    solved dark mode issue

--- scripts/interaction.js
+++ scripts/interaction.js
@@ -3,16 +3,11 @@ window.addEventListener("DOMContentLoaded", () => {
   let icon = document.getElementById("toggle");
   if (getUserPreference() == "light") {
     document.body.classList.toggle("light-theme")
-    icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
-</svg>`
+    icon.children[0].src = "/assets/svgs/dark-mode.svg";
   }
   else if (getUserPreference() == "dark") {
-    document.body.classList.toggle("light-theme")
-    icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
-</svg>
-`
+    document.body.classList.toggle("dark-theme")
+    icon.children[0].src = "/assets/svgs/light-mode.svg";
   }
 })
 
@@ -29,17 +24,13 @@ function toggle_dark_mode() {
   let icon = document.getElementById("toggle");
   if (getUserPreference() == "dark") {
     saveUserPreference("light")
-    icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
-</svg>`
     console.log(getUserPreference())
+    icon.children[0].src = "/assets/svgs/dark-mode.svg";
   }
   else if (getUserPreference() == "light") {
     saveUserPreference("dark")
     console.log(getUserPreference())
-    icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
-</svg>`
+    icon.children[0].src = "/assets/svgs/light-mode.svg";
   }
 }
 
